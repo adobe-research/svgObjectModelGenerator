41e3237981ff46064d8eb6e6cc28b159ae17d6a5
Josh Hatwich <jhatwich@gmail.com>
2014-08-21 14:18:33 -0500

    respond to layerComp requests
    all responses are the same so far.

--- svgWriterUtils.js
+++ svgWriterUtils.js
@@ -323,6 +323,101 @@
             return ctx.sOut;
         };
 
+        /** jQuery-style extend
+         *  https://github.com/jquery/jquery/blob/master/src/core.js
+         */
+        var jQueryLike = {
+          isFunction: function (obj) {
+            return jQueryLike.type(obj) === "function"
+          },
+          isArray: Array.isArray ||
+          function (obj) {
+            return jQueryLike.type(obj) === "array"
+          },
+          isWindow: function (obj) {
+            return obj != null && obj == obj.window
+          },
+          isNumeric: function (obj) {
+            return !isNaN(parseFloat(obj)) && isFinite(obj)
+          },
+          type: function (obj) {
+            return obj == null ? String(obj) : class2type[toString.call(obj)] || "object"
+          },
+          isPlainObject: function (obj) {
+            if (!obj || jQueryLike.type(obj) !== "object" || obj.nodeType) {
+              return false
+            }
+            try {
+              if (obj.constructor && !hasOwn.call(obj, "constructor") && !hasOwn.call(obj.constructor.prototype, "isPrototypeOf")) {
+                return false
+              }
+            } catch (e) {
+              return false
+            }
+            var key;
+            for (key in obj) {}
+            return key === undefined || hasOwn.call(obj, key)
+          }
+        };
+        
+        this.extend = function (deep, target, source) {
+            var options, name, src, copy, copyIsArray, clone, target = arguments[0] || {},
+                i = 1,
+                length = arguments.length,
+                deep = false,
+                toString = Object.prototype.toString,
+                hasOwn = Object.prototype.hasOwnProperty,
+                push = Array.prototype.push,
+                slice = Array.prototype.slice,
+                trim = String.prototype.trim,
+                indexOf = Array.prototype.indexOf,
+                class2type = {
+                  "[object Boolean]": "boolean",
+                  "[object Number]": "number",
+                  "[object String]": "string",
+                  "[object Function]": "function",
+                  "[object Array]": "array",
+                  "[object Date]": "date",
+                  "[object RegExp]": "regexp",
+                  "[object Object]": "object"
+                };
+            if (typeof target === "boolean") {
+                deep = target;
+                target = arguments[1] || {};
+                i = 2;
+            }
+            if (typeof target !== "object" && !jQueryLike.isFunction(target)) {
+                target = {}
+            }
+            if (length === i) {
+                target = this;
+                --i;
+            }
+            for (i; i < length; i++) {
+                if ((options = arguments[i]) != null) {
+                    for (name in options) {
+                        src = target[name];
+                        copy = options[name];
+                        if (target === copy) {
+                            continue;
+                        }
+                        if (deep && copy && (jQueryLike.isPlainObject(copy) || (copyIsArray = jQueryLike.isArray(copy)))) {
+                            if (copyIsArray) {
+                                copyIsArray = false;
+                                clone = src && jQueryLike.isArray(src) ? src : [];
+                            } else {
+                                clone = src && jQueryLike.isPlainObject(src) ? src : {};
+                            }
+                            target[name] = extend(deep, clone, copy);
+                        } else if (copy !== undefined) {
+                            target[name] = copy;
+                        }
+                    }
+                }
+            }
+            return target;
+        };
+        
         this.toBase64 = function (string) {
             var buf = new Buffer(string);
             return buf.toString("base64");
