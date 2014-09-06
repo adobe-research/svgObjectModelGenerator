14b109232a4af51590de27b7b337ca7f4eb95f64
Josh Hatwich <jhatwich@gmail.com>
2014-09-06 15:22:35 -0500

    survive presence of empty layers

--- svgOMGenerator.js
+++ svgOMGenerator.js
@@ -1,17 +1,17 @@
-/*
- * ADOBE CONFIDENTIAL
- *
- * Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
- *
- * NOTICE:  All information contained herein is, and remains
- * the property of Adobe Systems Incorporated and its suppliers,
- * if any.  The intellectual and technical concepts contained
- * herein are proprietary to Adobe Systems Incorporated and its
- * suppliers and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from Adobe Systems Incorporated.
- */
+// Copyright (c) 2014 Adobe Systems Incorporated. All rights reserved.
+// 
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 
 /*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, bitwise: true */
 /*global define: true, require: true, module: true */
@@ -113,7 +113,7 @@
     
     function layerShouldBeRasterized(layer) {
         var layerType = getSVGLayerType(layer);
-        return layerType === "generic";
+        return (layerType === "generic" && layer.bounds.right - layer.bounds.left > 0 && layer.bounds.bottom - layer.bounds.top > 0);
     }
     
     function extractSVGOM (psd, opts) {
