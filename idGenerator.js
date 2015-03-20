a80af3e2b92d8c6b96ecda6635ac10cb6eaf1e67
Dirk Schulze <dschulze@chromium.org>
2015-03-20 17:54:15 +0100

    Typos

--- idGenerator.js
+++ idGenerator.js
@@ -51,8 +51,8 @@
         };
 
         var uniqueId = function () {
-            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
-                    var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
+            return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function (c) {
+                    var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 3| 8);
                     return v.toString(16);
                 });
         };
