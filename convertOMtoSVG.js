2f33c7b629354f5aa0ce4b8422265ad4585994f4
Dmitry Baranovskiy <baranovs@adobe.com>
2015-04-16 17:57:32 +1000

    Added support for options file to convertOMtoSVG script.

--- convertOMtoSVG.js
+++ convertOMtoSVG.js
@@ -1,13 +1,24 @@
 var fs = require("fs");
 
 if (process.argv.length < 4) {
-    console.log("Usage: node convertOMtoSVG.js <inputOMFile> <outputSVGFile> [<width> <height>]");
+    console.log("Usage: node convertOMtoSVG.js <inputOMFile> [<optionsFile>] <outputSVGFile>");
     throw new Error();
 }
 
 var inFile = process.argv[2],
     outFile = process.argv[3],
-    svgOMString = fs.readFileSync(inFile, {"encoding": "utf8", "flag": "r"}),
+    optFile = process.argv[4];
+if (optFile) {
+    outFile = process.argv[4];
+    optFile = process.argv[3];
+}
+var svgOMString = fs.readFileSync(inFile, {encoding: "utf8", flag: "r"}),
+    options = optFile ? JSON.parse(fs.readFileSync(optFile, {encoding: "utf8", flag: "r"})) : {
+        trimToArtBounds: false,
+        preserveAspectRatio: "xMidYMid",
+        scale: 1,
+        constrainToDocBounds: false
+    }
     svgOM = JSON.parse(svgOMString);
 if ("version" in svgOM) {
     var opt = [],
@@ -19,32 +30,15 @@ if ("version" in svgOM) {
     }
     svgOM = OMG.extractSVGOM(svgOM, opt);
 }
-var viewBox = svgOM.global ? svgOM.global.viewBox : null,
-    targetWidth = viewBox ? viewBox.right - viewBox.left : 612,
-    targetHeight = viewBox ? viewBox.bottom - viewBox.top : 792;
-
-if (process.argv.length > 4 && parseFloat(process.argv[4]) > 0) {
-    targetWidth = parseInt(process.argv[4], 10);
-}
-if (process.argv.length > 5 && parseFloat(process.argv[5]) > 0) {
-    targetHeight = parseInt(process.argv[5], 10);
-}
 
 var svgWriter = require("./svgWriter.js"),
     svgWriterErrors = [],
-    svgOut = svgWriter.printSVG(svgOM, {
-                                    trimToArtBounds: false,
-                                    preserveAspectRatio: "xMidYMid",
-                                    scale: 1,
-                                    targetWidth: targetWidth,
-                                    targetHeight: targetHeight,
-                                    constrainToDocBounds: false
-                                }, svgWriterErrors);
+    svgOut = svgWriter.printSVG(svgOM, options, svgWriterErrors);
 
 for (var i = 0; i < svgWriterErrors.length; i++) {
     console.error(svgWriterErrors[i]);
 }
 
 fs.writeFileSync(outFile, svgOut);
-console.log("Done (" + targetWidth + " × " + targetHeight + ").");
+console.log("<···Done···>");
 console.log("Check out the result: " + outFile);
